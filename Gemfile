6460ef5a69873efbf3e2ff9e46d1d0c33834795f
Tomasz Szkaradek <tmster.sz@gmail.com>
2020-01-06 19:50:38 +0100

    Add seeds

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'rubocop-rspec'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'ffaker'
   gem 'rspec-rails'
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,35 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'net/http'
+require 'json'
+require 'ffaker'
+
+def fetch(url, limit = 10)
+  # You should choose better exception.
+  raise ArgumentError, 'HTTP redirect too deep' if limit.zero?
+
+  uri = URI.parse(url)
+  req = Net::HTTP::Get.new(uri)
+  response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) { |http| http.request(req) }
+  case response
+  when Net::HTTPSuccess     then response
+  when Net::HTTPRedirection then fetch(response['location'], limit - 1)
+  else
+    response.error!
+  end
+end
+
+response = fetch('https://picsum.photos/v2/list?page=1&limit=100')
+data = JSON.parse(response.body)
+
+data.each do |row|
+  puts "Downloading: #{row['download_url']}"
+  image_contents = fetch(row['download_url'])
+  picture = Picture.new(title: FFaker::Book.title,
+                        description: FFaker::Book.description)
+  io = StringIO.new(image_contents.body)
+
+  picture.image.attach(io: io, filename: "temp.#{row['id']}.jpg", content_type: image_contents.content_type)
+
+  picture.save!
+end
