6fbfdd5e0c0c6e5969d47bac81b33de3d0185810
Tomasz Szkaradek <tmster.sz@gmail.com>
2020-01-12 21:11:57 +0100

    Delete all unnecessary files

--- app/javascript/components/__tests__/HelloWorld.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-
-import HelloWorld from '../HelloWorld';
-
-it('renders name', () => {
-  const node = shallow(<HelloWorld greeting="example" />);
-  expect(node).toMatchSnapshot();
-});
